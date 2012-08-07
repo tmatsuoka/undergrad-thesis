257516aba927f4f6d967823390a1b01b121319b3
Takashi Matsuoka <takashim@cse.unsw.edu.au>
2012-08-07 17:06:25 +1000

    Improve the definition (though very messy) by adding basic definitions for security policy.
    This implementation closely follows Rushby; you supply security policy by definiting the function,
    "interferes" (Domain -> Domain -> Bool).

--- system.hs
+++ system.hs
@@ -1,39 +1,35 @@
 -- Rushby's finite system (essentially DFA/NFA)
 
-module System where
+module SecuritySystem where
 
 data State = State { id_s :: Int } deriving (Show, Eq)
-
 data Action = Action { id_a :: Int } deriving (Show, Eq)
-
 newtype Output = Output Char deriving (Show, Eq)
 
 data System = System { states :: [State],
                        actions :: [Action],
                        initial :: State,
                        step :: State -> Action -> State,
-                       output :: State -> Action -> Output
+                       output :: State -> Action -> Output,
+                       dom :: Action -> Domain
                      }
 
 run :: System -> State -> [Action] -> State
 run _ s [] = s
-run sys@(System _ _ _ step _) s (a:as) = run sys (step s a) as
+run sys s (a:as) = run sys ((step sys) s a) as
 
-s1 = State 1
-s2 = State 2
-s3 = State 3
-a1 = Action 1
-a2 = Action 2
+do_run :: System -> [Action] -> State
+do_run sys as = run sys (initial sys) as
 
-sys = System [s1, s2, s3] [a1, a2] s1 ex_sys_step ex_sys_output
-    where ex_sys_step s a | s == s1 && a == a1 = s2
-                          | s == s1 && a == a2 = s3
-                          | s == s2 = s3
-                          | s == s3 && a == a1 = s3
-                          | s == s3 && a == a2 = s1
-          ex_sys_output s a | s == s1 && a == a1 = Output 'h'
-                            | s == s1 && a == a2 = Output 'e'
-                            | s == s2 = Output 'l'
-                            | s == s3 && a == a1 = Output 'l'
-                            | s == s3 && a == a2 = Output 'o'
+test :: System -> [Action] -> Action -> Output
+test sys as a = output sys (do_run sys as) a
+
+data Domain = Domain { id_d :: Char }
+
+purge :: System -> (Domain -> Domain -> Bool) -> [Action] -> Domain -> [Action]
+purge sys inter (a:as) v | ((dom sys) a) `inter` v = a : (purge sys inter as v)
+                         | otherwise               = purge sys inter as v
+
+partial_secure :: System -> (Domain -> Domain -> Bool) -> [Action] -> Action -> Bool
+partial_secure sys inter as a = test sys as a == test sys (purge sys inter as ((dom sys) a)) a
 
