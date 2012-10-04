6781dee06fea3c2826b819c93f4190b995f9aab5
Takashi Matsuoka <takashim@cse.unsw.edu.au>
2012-10-04 14:46:28 +1000

    Generate executable action sequences only

--- SecurityChecker.hs
+++ SecurityChecker.hs
@@ -19,11 +19,6 @@ import ArbitrarySystem
 
 import qualified SSDLLite as EDSL
 
-{-
-instance (GenSingleton m, GenSingleton n) => Arbitrary (NatSet m, NatSet n) where
-    arbitrary = return (arbitrary :: Gen (GenSingleton m => NatSet m), arbitrary :: Gen (NatSet n))
--}
-
 prop_purge_secure :: System s a d -> Domain d -> [Action a] -> Property
 prop_purge_secure sys d as = 
     not (List.null as) ==> not (isNothing state) ==> not (isNothing purged_state) ==> obs_f (fromJust state) d == obs_f (fromJust purged_state) d
@@ -38,17 +33,12 @@ prop_ipurge_secure sys d as =
           purged_state = doRun sys (ipurge sys (policy sys) as d)
           obs_f        = obs sys
 
--- securityCheckEDSL :: EDSL.ExistsEDSLSystem -> IO ()
-securityCheckEDSL (EDSL.ExES sys) = quickCheck (prop_purge_secure b_sys)
-    where b_sys = EDSL.getBase sys
-
 -- getCase: Given a system, generates a random security domain and action sequence.
--- TODO: Make this so that it always gives out executable action sequences.
 
 getCase :: (GenSingleton a, GenSingleton d) => System s a d -> Gen (Domain d, [Action a])
 getCase sys = do
     domain  <- arbitrary :: GenSingleton d => Gen (NatSet d)
-    actions <- listOf (arbitrary :: GenSingleton a => Gen (NatSet a))
+    actions <- suchThat (listOf $ (arbitrary :: GenSingleton a => Gen (NatSet a))) (\as -> not $ isNothing $ doRun sys as)
     return (domain, actions)
 
 -- Test property which checks that all systems must be both P-secure and IP-secure. Clearly this is false (run it and see!)
